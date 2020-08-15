1043b07405a2f381a92037f6f485cac28bf4acf6
elkowar <5300871+elkowar@users.noreply.github.com>
2020-08-16 00:48:11 +0200

    update default.nix to use different build method

--- default.nix
+++ default.nix
@@ -1,10 +1,5 @@
-{ pkgs ? import <nixpkgs> {}
-}: {
-  rnix-lsp = pkgs.rustPlatform.buildRustPackage rec {
-    pname = "rnix-lsp";
-    version = "0.1.0";
-    src = ./.;
-    cargoSha256 = "021zcdz3dynrm78hips0jyf0xfkrxa0f28swalgx7vqz6jj8yi8a";
-  };
-}
-
+{
+  sources ? import ./nix/sources.nix,
+  pkgs ? import <nixpkgs> {},
+  naersk ? pkgs.callPackage sources.naersk {},
+}: naersk.buildPackage ./.
